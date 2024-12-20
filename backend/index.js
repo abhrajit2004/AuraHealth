9489deae3c3ea15eedb27482cfb6cb6c04cd131d
unknown <abhrajitgupta.it2022@nsec.ac.in>
2024-12-20 22:43:06 +0530

    created medical shop feature

--- backend/index.js
+++ backend/index.js
@@ -9,19 +9,41 @@ app.use(cors())
 
 app.use(express.json())
 
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
 app.post('/gettreatment', async (req, res) => {
 
     const disease = req.body.disease;
 
-    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const prompt = `provide the necessary actions for ${disease} in less sentences`;
 
     const result = await model.generateContent(prompt);
-    console.log(result.response.text());
+
     res.json({ success: true, message: 'Successfully fetched the treatment!', treatment: result.response.text() });
 
+})
+
+app.post('/getmedicines', async (req, res) => {
+
+    const healthProblem = req.body.healthProblem;
+
+    const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const prompt = `provide the necessary medicines in array of json format with price ( only in INR ) and id like uuid for ${healthProblem}. Don't give any disclaimer or note at the end of array. JSON format should be like this:
+
+     {    "id": "a1b2c3d4-e5f6-7890-1234-567890abcdef",    "medicine": "Omeprazole",    "price": 20  }
+    `;
+
+    const result = await model.generateContent(prompt);
+
+    const modifiedResult = result.response.text().split('\n').join('').replace('```json', '').replace('```', '');  
+
+    res.json({ success: true, message: 'Successfully fetched the treatment!', medicines: modifiedResult });
 })
 
 app.get('/', (req, res) => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import PatientRecord from './components/PatientRecord'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HealthRecords from './components/HealthRecords'
 import { Link } from 'react-router-dom'
+import MedicalShop from './components/MedicalShop'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,11 +14,15 @@ function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <div className='flex justify-center items-center min-h-screen flex-col gap-10'><span className='font-bold text-4xl'>Visit the following buttons</span><div className='buttons'><Link to={"/register"} className='bg-blue-700 text-white px-4 py-4 rounded-lg transition hover:bg-blue-600 w-[15vw] font-bold'>Register as patient</Link></div></div>
+      element: <div className='flex justify-center items-center min-h-screen flex-col gap-10'><span className='font-bold text-4xl'>Visit the following buttons</span><div className='buttons'><Link to={"/register"} className='bg-blue-700 text-white px-4 py-4 rounded-lg transition hover:bg-blue-600 w-[15vw] font-bold'>Register as patient</Link><Link to={"/medicalshop"} className='bg-blue-700 text-white px-4 py-4 ml-4 rounded-lg transition hover:bg-blue-600 w-[15vw] font-bold'>Visit Medical Shop</Link></div></div>
     },
     {
       path: '/register',
       element: <PatientRecord />  
+    },
+    {
+      path: '/medicalshop',
+      element: <MedicalShop />  
     },
     {
       path: "/patientrecords/:id",
